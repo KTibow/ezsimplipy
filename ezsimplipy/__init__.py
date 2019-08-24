739a1607b1eaf6afd17bcdf53e6ad69a9da3e70b
kendell <kendell.r@outlook.com>
2019-08-24 09:56:00 -0700

    Update __init__.py

--- ezsimplipy/__init__.py
+++ ezsimplipy/__init__.py
@@ -1,2 +1,14 @@
 from pip._internal import main as pipmain
-pipit = lambda args: pipmain(args.split(" "))
+pipit = lambda theargs: pipmain(theargs.split(" "))
+class VersionError(Exception):
+  pass
+def makeSure(libname):
+  try:
+    import importlib
+  except ImportError:
+    raise VersionError("Please upgrade your Python interpreter.")
+  try:
+    importlib.import_module(libname)
+  except ModuleNotFoundError:
+    pipit("install "+libname)
+    importlib.import_module(libname)
