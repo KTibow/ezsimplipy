889badc66983c2b7647fb330c37f0dfbd1ae591e
kendell <kendell.r@outlook.com>
2019-08-24 10:01:16 -0700

    Update __init__.py

--- ezsimplipy/__init__.py
+++ ezsimplipy/__init__.py
@@ -1,14 +1,21 @@
 from pip._internal import main as pipmain
 pipit = lambda theargs: pipmain(theargs.split(" "))
 class VersionError(Exception):
-  pass
+	pass
 def makeSure(libname):
-  try:
-    import importlib
-  except ImportError:
-    raise VersionError("Please upgrade your Python interpreter.")
-  try:
-    importlib.import_module(libname)
-  except ModuleNotFoundError:
-    pipit("install "+libname)
-    importlib.import_module(libname)
+	try:
+		import importlib
+	except ImportError:
+		raise VersionError("Please upgrade your Python interpreter.")
+	try:
+		importlib.import_module(libname)
+	except ModuleNotFoundError as e:
+		try:
+			pipit("install "+libname)
+			importlib.import_module(libname)
+			return (0, e, -1)
+		except Exception as f:
+			return (-1, e, f)
+makeSure("asyncio")
+makeSure("aiohttp")
+ClientSession = aiohttp.ClientSession
